c558ab4b9eafa070a81e48054e11d08ed367a88e
pole wang <w_king0831@163.com>
2026-01-29 10:04:30 +0800

    实现数据库

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,5 +9,6 @@ from web.views.index import index
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('',index)
+    path('',index),
+    re_path(r'^(?!media/|static/|assets/).*$', index)
 ]
