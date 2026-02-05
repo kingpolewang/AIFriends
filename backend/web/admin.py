2c2696465a7f1875784e2be7d45403bde95e7d83
pole wang <w_king0831@163.com>
2026-02-05 16:14:02 +0800

    实现Character数据库，并加入到后台管理系统中

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,8 +1,16 @@
+# 1. 导入Django的后台管理模块
 from django.contrib import admin
+# 2. 导入你的自定义模型
+from web.models.character import Character
 from web.models.user import UserProfile
 
-
 # Register your models here.
+# 4. 用装饰器方式注册UserProfile模型
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ('user',)    #逗号不能删
+    # 5. 设置user字段为   raw_id_fields--(输入框搜索)
+    raw_id_fields = ('user',)    # 逗号不能删 - 因为要创建元组(tuple)
+
+@admin.register(Character)      #把Character模型注册到Django后台
+class CharacterAdmin(admin.ModelAdmin): #定义Character模型的管理配置类
+    raw_id_fields = ('author',)     # raw_id_fields让author字段用输入框而不是下拉框显示
---
+++ backend/web/models/character.py
@@ -0,0 +1,45 @@
+import os
+import uuid
+from time import localtime
+
+from django.db import models
+from django.utils import timezone
+from web.models.user import UserProfile
+# 上传图片处理函数
+def photo_upload_to(instance, filename):
+    # 1. 获取文件扩展名（如'jpg', 'png'）
+    ext = filename.split('.')[-1]
+    # 2. 生成10位随机字符串作为新文件名
+    filename = f'{uuid.uuid4().hex[:10]}.{ext}'
+    # 3. 返回存储路径：character/photos/用户ID_随机文件名
+    return f'character/photos/{instance.author.user_id}_{filename}'
+
+
+def background_image_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4().hex[:10]}.{ext}'
+    return f'character/backgrounds_images/{instance.author.user_id}_{filename}'
+
+class Character(models.Model):  # models.Model----继承Django的Model基类
+    # 外键关联：一个Character属于User下的UserProfile（建立模型之间的关系）
+    # on_delete=models.CASCADE: 当用户被删除时，其所有角色也被删除
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    # 角色名称：最大50字符的文本字段
+    name = models.CharField(max_length=50)
+    # 角色照片：图片字段，上传时调用photo_upload_to函数
+    photo =models.ImageField(upload_to=photo_upload_to)
+    # 角色简介：长文本字段，最大100000字符
+    profile = models.TextField(max_length=100000)
+    # 背景图片 上传时调用background_image_upload_to函数
+    background_image = models.ImageField(upload_to=background_image_upload_to)
+
+    # 创建时间：自动设为当前时间（只在创建时设置）
+    create_time = models.DateTimeField(default=timezone.now)
+    update_time = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        """
+           定义模型的字符串表示
+           在Django后台、shell等地方显示时使用
+        """
+        # 格式：用户名 - 角色名 - 创建时间
+        return f"{self.author.user.username} - {self.name} - {localtime(self.create_time).strftime('%Y-%m-%d %H:%M:%S')}"
