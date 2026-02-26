73abe9821e720497c7bc60031152cc5ee210e0f8
pole wang <w_king0831@163.com>
2026-02-26 10:36:35 +0800

    实现friend数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 # 2. 导入你的自定义模型
 from web.models.character import Character
+from web.models.friend import Friend
 from web.models.user import UserProfile
 
 # Register your models here.
@@ -14,3 +15,7 @@ class UserProfileAdmin(admin.ModelAdmin):
 @admin.register(Character)      #把Character模型注册到Django后台
 class CharacterAdmin(admin.ModelAdmin): #定义Character模型的管理配置类
     raw_id_fields = ('author',)     # raw_id_fields让author字段用输入框而不是下拉框显示
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me','character')
