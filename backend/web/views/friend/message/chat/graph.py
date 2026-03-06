ad9c5141ddac7ff5e6aa0b76be74b6dfe05146fc
pole wang <w_king0831@163.com>
2026-03-06 22:24:05 +0800

    实现LangGraph的工具节点调用--即实现function call

--- backend/web/views/friend/message/chat/graph.py
+++ backend/web/views/friend/message/chat/graph.py
@@ -1,10 +1,14 @@
 import os
+from pprint import pprint
 from typing import TypedDict, Annotated, Sequence
 
+from django.utils.timezone import localtime, now
 from langchain_core.messages import BaseMessage
+from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langgraph.constants import START, END
 from langgraph.graph import add_messages, StateGraph
+from langgraph.prebuilt import ToolNode
 
 
 #对接大模型
@@ -13,14 +17,19 @@ class ChatGraph:
     def create_app():
         """
                创建并返回一个编译好的LangGraph应用
-
                这个方法构建了一个简单的对话图结构：
                1. 初始化大语言模型（使用DeepSeek）
                2. 定义Agent的状态类型（包含消息列表）
                3. 创建模型调用节点
-               4. 构建图结构：START -> agent -> END
+               4. 构建图结构：START -> agent -> (tool -> agnet ->) END
         """
 
+        # 装饰器 @tool
+        @tool
+        def get_time() -> str:
+            """查询精确时间时，调用此函数。返回格式为：[年-月-日-时-分-秒]"""
+            return localtime(now()).strftime("%Y-%m-%d %H:%M:%S")
+        tools=[get_time]
         # 初始化大语言模型客户端
         # ChatOpenAI 兼容OpenAI API格式的模型，这里使用的是DeepSeek
         llm=ChatOpenAI(
@@ -32,8 +41,8 @@ class ChatGraph:
                 "stream_options": {
                     "include_usage": True,  # 输出token消耗数量
                 }
-            }
-        )
+            }                   #.bind_tool(tools) 是 LangChain 框架中的一个方法，用于将工具函数绑定到 LLM 模型实例上
+        ).bind_tools(tools)      #将定义好的工具（tools）绑定到大语言模型，使模型具备调用这些工具的能力。
 
         # 定义Agent的状态类型
         # TypedDict 是Python的类型提示，用于定义字典的结构
@@ -52,11 +61,24 @@ class ChatGraph:
                 参数: state: 当前的Agent状态，包含消息列表
                 返回:更新后的状态，包含模型生成的回复消息
             """
+            pprint(state)
             # 调用大语言模型，传入当前状态中的所有消息
             res = llm.invoke(state['messages'])
             return {
                 'messages': [res],
             }
+
+        #路由函数
+        def should_continue(state: AgentState) -> str:
+            # 1. 获取最后一条消息
+            last_message = state['messages'][-1]
+            # 2. 检查是否有工具调用
+            if last_message.tool_calls: # 如果有工具调用
+                return "tools"          # 下一步执行工具节点
+            return "end"                # 结束
+
+        # 创建工具节点
+        tool_node = ToolNode(tools)
 
         # 创建状态图
         # StateGraph 是LangGraph的核心类，用于构建状态图
@@ -64,13 +86,25 @@ class ChatGraph:
         # 添加节点
         # add_node(节点名称, 节点函数)
         graph.add_node('agent',model_call)
+        graph.add_node('tools',tool_node)
 
         # 添加边连接
         # START是特殊的起始节点，指向agent节点
         graph.add_edge(START, 'agent')
-        # agent节点指向END（结束节点）
-        graph.add_edge('agent',END)
+        # # agent节点指向END（结束节点）
+        # graph.add_edge('agent',END)
 
+        # 构建条件边
+        graph.add_conditional_edges(
+            'agent',        #从agent节点出发
+            should_continue,       # 路由函数：决定下一步去哪
+            {             #映射表：函数返回值 -> 目标节点
+                'tools':'tools',     # 如果返回"tools"，去tools节点
+                'end': END,         # 如果返回"end"，结束流程
+            }
+        )
+        # 构建固定边
+        graph.add_edge('tools', 'agent')
         # 编译图并返回
         # compile()会将定义的图结构编译成可执行的应用
         return graph.compile()
